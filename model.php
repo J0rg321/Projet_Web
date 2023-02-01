1070248ba0a9e5d994a7bbebd4a08cf224d3138b
MAVRAJ Alessia <Alessia.MAVRAJ@cpnv.ch>
2023-02-01 15:32:48 +0100

    Ajout ligne pour le logo

--- model.php
+++ model.php
@@ -7,6 +7,25 @@
 <link rel="stylesheet" href="style.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-
-
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="LogoWear.PNG" alt="WearYou" style="width:40px;" class="rounded-pill">
+        </a>
+    </div>
+</nav>
+<ul class="nav nav-tabs">
+    <li class="nav-item">
+        <a class="nav-link active" href="#">Active</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+    </li>
+</ul>
 </html>
