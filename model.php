03c739d181427f1962f821261a2cf9ef991f1de4
MAVRAJ Alessia <Alessia.MAVRAJ@cpnv.ch>
2023-02-01 15:24:35 +0100

    ajout du contenu HTML dans le model

--- model.php
+++ model.php
@@ -1,1 +1,36 @@
+
+
+<html>
 <?php
+
+?>
+<link rel="stylesheet" href="style.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="LogoWear.PNG" alt="Entreprise logo" style="width:100px;" class="rounded-pill"> <!--Création d'une bannière avec le logo de l'entreprise-->
+        </a>
+    </div>
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link active" data-bs-toggle="tab" href="#home">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="tab" href="#menu1">Menu 1</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="tab" href="#menu2">Menu 2</a>
+        </li>
+    </ul>
+
+    <!-- Tab panes -->
+    <div class="tab-content">
+        <div class="tab-pane container active" id="home">...</div>
+        <div class="tab-pane container fade" id="menu1">...</div>
+        <div class="tab-pane container fade" id="menu2">...</div>
+    </div>
+</nav>
+</html>
