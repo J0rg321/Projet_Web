02fb38151613738cef3d08ba84bb77f30180b828
MAVRAJ Alessia <Alessia.MAVRAJ@cpnv.ch>
2023-02-01 15:24:54 +0100

    lien bootstrap

--- gabarit.php
+++ gabarit.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="utf-8">
     <title>Gabarit</title>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
 <?php require('model.php') ?>
