d817812a9d27439b92a4edae536dc6f1e697c7a2
MAVRAJ Alessia <Alessia.MAVRAJ@cpnv.ch>
2023-02-01 15:55:24 +0100

    ajout de la liaison pour le fichier css

--- model.php
+++ model.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>Model</title>
 </head>
-
+<link href="style.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 <body>
