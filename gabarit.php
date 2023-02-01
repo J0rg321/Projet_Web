cbdd210a5046b35e9cc7af76ebadff118b7df86a
MAVRAJ Alessia <alessia.mavraj@cpnv.ch>
2023-02-01 14:39:13 +0100

    ajout de la structure HTML

---
+++ gabarit.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Gabarit</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+</body>
+<?php
+
+?>
+
+</html>
+
+
