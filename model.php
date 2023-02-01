90da79519b0a728aeef086286d47afa26939409f
MAVRAJ Alessia <Alessia.MAVRAJ@cpnv.ch>
2023-02-01 15:40:05 +0100

    15h39 - 01.02.2023

--- model.php
+++ model.php
@@ -7,22 +7,25 @@
 <link rel="stylesheet" href="style.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+<nav class="navbar navbar-expand-sm navbar-dark">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">
             <img src="LogoWear.PNG" alt="WearYou" style="width:100px;" class="rounded-pill">
         </a>
     </div>
 </nav>
-<ul class="nav nav-tabs">
-    <li class="nav-item">
-        <a class="nav-link active" href="#">Vêtements</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link active" href="#">Soldes</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link active" href="#">Vendeurs phare</a>
-    </li>
-</ul>
+<div>
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link active" href="vetements.ch">Vêtements</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" href="soldes.ch">Soldes</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" href="vendeurs.ch">Vendeurs phare</a>
+        </li>
+    </ul>
+</div>
+
 </html>
