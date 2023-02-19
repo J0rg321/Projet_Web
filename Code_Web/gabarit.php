6a604b5f4483dc12be8a345d95d56b5ed50d5a06
J0rg321 <98883887+J0rg321@users.noreply.github.com>
2023-02-19 12:14:10 +0100

    Yes

---
+++ Code_Web/gabarit.php
@@ -0,0 +1,3 @@
+<?php
+    require('model.php') //appel du fichier model pour avoir la structure du site
+?>
---
+++ Code_Web/model.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Model</title>
+</head>
+<link href="style.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<body>
+<nav class="navbar navbar-expand-sm navbar-dark">
+    <div class="container-fluid">
+            <img src="Images/LogoWear.PNG" alt="WearYou" style="width:100px" class="rounded-pill">
+    </div>
+</nav>
+<div id="navigation">
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link active" href="vetements.html">Vêtements</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" href="soldes.html">Soldes</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" href="vendeurs.html">Vendeurs phare</a>
+        </li>
+    </ul>
+</div>
+</body>
+</html>
