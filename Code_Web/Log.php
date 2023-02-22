c6d3e5bdf003332f46509c78a8830e936ec6c20d
MAVRAJ Alessia <Alessia.MAVRAJ@cpnv.ch>
2023-02-22 15:39:37 +0100

    ajout de la fonction pour définir le log

--- Code_Web/Log.php
+++ Code_Web/Log.php
@@ -1,1 +1,10 @@
 <?php
+/**
+ * User: Alessia Mavraj
+ * Date: 22.02.2023
+ * Time: 15h28
+ * Title : Fonction de login pour la création de compte / login
+ * Project : Projet_Web
+ */
+
+$fichierLog = ".log";//define log file name
