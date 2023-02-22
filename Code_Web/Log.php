1b0dc264fb05f81d5c84ce8108e8da4d7b1b5580
MAVRAJ Alessia <Alessia.MAVRAJ@cpnv.ch>
2023-02-22 15:44:24 +0100

    modif 22.02.2023

--- Code_Web/Log.php
+++ Code_Web/Log.php
@@ -7,4 +7,9 @@
  * Project : Projet_Web
  */
 
-$fichierLog = ".log";//define log file name
+$fichierLog = "stockage des logins.log";//définition du fichier de log
+$fileFullPath = setFullPath($fichierLog);//définition du chemin entier jusqu'au fichier
+$logHeader = "Temps du log / création\t\t\tNom utilisateur";//affiche les entêtes du fichier
+
+
+echo setFullPath("stockage des logins.log") . "<br>"; //test pour détection du fichier
