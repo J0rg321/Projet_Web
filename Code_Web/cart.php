af682660c92f081136f4ecc83cf3f6e4df8a24c8
MAVRAJ Alessia <Alessia.MAVRAJ@cpnv.ch>
2023-03-02 13:45:41 +0100

    ajout symbole (test)

--- Code_Web/cart.php
+++ Code_Web/cart.php
@@ -52,7 +52,8 @@
                 <div class="d-flex justify-content-end">
                     <div class="col-6">
                         <a href="cart.php">
-                            <img class="nav-link icon" src="Images/trolley.png" aria-current="page" style="width:100px" alt="cart de shopping">
+                            <img class="nav-link icon" src="Images/trolley.png" aria-current="page" style="width:100px"
+                                 alt="cart de shopping">
                         </a>
                     </div>
                     <div class="col-6 account">
@@ -66,7 +67,10 @@
     </div>
 </div>
 <footer>
-<a>jogjipggf</a>
+    <a>Ici il y a aura un footer plein</a>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+    </svg>
 </footer>
 </body>
 </html>
