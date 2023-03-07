34dc038b098e38407526ebd62df84d1f428f157d
Jorge Miguel <jorge-miguel.pinto-costa@cpnv.ch>
2023-03-07 16:53:04 +0100

    Update

--- Code_Web/index.php
+++ Code_Web/index.php
@@ -1,1 +1,27 @@
 <?php
+
+session_start();
+require "controller/navigation.php";
+require "controller/Snow.php";
+
+if (isset($_GET['action'])) {
+  $action = $_GET['action'];
+  switch ($action) {
+	case 'accueil' :
+	  accueil();
+	  break;
+	case 'vetements' :
+	  vetements();
+
+	case 'cart' :
+	  cart();
+
+	case 'compte' :
+	  compte();
+
+	default :
+	  accueil();
+  }
+} else {
+  home();
+}
