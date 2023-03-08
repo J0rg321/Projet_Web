38a56e6522fbf2fae71646ebdda0d86fb9030e4a
MAVRAJ Alessia <Alessia.MAVRAJ@cpnv.ch>
2023-03-08 16:06:06 +0100

    création d'un fichier json pour les différentes valeurs pour les articles du site

---
+++ Code_Web/statics/js/dara.js
@@ -0,0 +1,14 @@
+[
+    {
+        "Casquette_n": {
+            "id": 1,
+            "taille": "unique",
+            "couleur": "black",
+            "prix":9.90,
+            "genre":"mixte"
+        }
+
+
+    }
+]
+
