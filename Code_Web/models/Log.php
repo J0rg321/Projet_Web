b5faca797e0bf2a91988c6abade2c0d09fe00dbb
MAVRAJ Alessia <Alessia.MAVRAJ@cpnv.ch>
2023-03-09 14:12:50 +0100

    ajout en tête

--- Code_Web/models/Log.php
+++ Code_Web/models/Log.php
@@ -1,1 +1,10 @@
 <?php
+/**
+ * Projet   : Projet_Web
+ * Fichier  : log.php
+ * Auteur   : Alessia Mavraj
+ * Date     : 09.03.2023 14h11
+ * Version  : 1.0.0
+ * Description: Ce fichier fera en sorte de récupérer les données des nouveaux clients / clients existants
+ */
+
