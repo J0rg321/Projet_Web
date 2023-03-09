eb74814eea36e79883031033d5ce19650a844f4e
MAVRAJ Alessia <Alessia.MAVRAJ@cpnv.ch>
2023-03-09 14:37:50 +0100

    modification du create

--- Code_Web/views/create.php
+++ Code_Web/views/create.php
@@ -4,29 +4,31 @@ $title = "Creation de compte";
 $categories = '';
 ?>
     <div class="div_centre">
-        <h1>Veuillez vous logger : </h1>
+        <h1>Veuillez créer votre compte : </h1>
         <form action="C:\GitHub\Projet_Web\Code_Web\statics\js\user.json" method="post">
             <div class="form-group">
-                <label for="pseudo">Pseudo *</label>
-                <input type="text" id="pseudo" aria-describedby="pseudoHelp" placeholder="Enter pseudo"
+                <label for="pseudo">Pseudo*</label>
+                <input type="text" id="pseudo" aria-describedby="pseudoHelp" placeholder="Votre pseudo"
                        class="form-control" required>
             </div>
             <div class="form-group">
-                <label for="userName">User Name *</label>
-                <input type="text" id="userName" aria-describedby="userNameHelp" placeholder="Enter user name"
+                <label for="userName">Nom d'utilisateur*</label>
+                <input type="text" id="userName" aria-describedby="userNameHelp" placeholder="Votre nom d'utilisateur"
                        class="form-control" required>
             </div>
             <div class="form-group">
-                <label for="inputEmail">Email address *</label>
-                <input type="email" id="inputEmail" aria-describedby="emailHelp" placeholder="firstname@domain.ch"
+                <label for="inputEmail">Adresse-mail*</label>
+                <input type="email" id="inputEmail" aria-describedby="emailHelp" placeholder="Votre adresse mail"
                        class="form-control" required>
             </div>
             <div class="form-group">
-                <label for="inputPassword">Password *</label>
-                <input type="password" id="inputPassword" placeholder="Password" class="form-control" required>
+                <label for="inputPassword">Mot de passe*</label>
+                <input type="password" id="inputPassword" placeholder="Votre mot de passe" class="form-control"
+                       required>
             </div>
+            <div id="envoi"> <button type="submit" class="btn btn-primary">Envoyer</button> </div>
+                <button type="reset" class="btn btn-primary">Réinitialiser</button>
 
-            <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
 <?php
