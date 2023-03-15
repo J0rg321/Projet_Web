378fd8b8bc855ab8d6dc4bd9cfdd6787481887ae
AlessiaMavraj <alessia.mavraj@cpnv.ch>
2023-03-15 15:36:15 +0100

    ajout du code pour récupérer les données

--- Code_Web/views/create.php
+++ Code_Web/views/create.php
@@ -2,19 +2,18 @@
 ob_start();
 $title = "Creation de compte";
 $categories = '';
-$users = file_get_contents("users.json");
 
-function writedata()
-{ //création d'une fonction pour écrire les données du form dans le .json
-
+if (isset($_POST['envoi'])){
+    echo $_POST['pseudo'];
 }
+
 ?>
     <div class="div_centre">
         <h1>Veuillez créer votre compte : </h1>
         <form action='user.json' method='post'>
             <div class="form-group">
                 <label for="pseudo">Pseudo*</label>
-                <input type="text" id="pseudo" aria-describedby="pseudoHelp" placeholder="Votre pseudo"
+                <input type="text" name='pseudo' id="pseudo" aria-describedby="pseudoHelp" placeholder="Votre pseudo"
                        class="form-control" required>
             </div>
             <div class="form-group">
@@ -33,7 +32,7 @@ function writedata()
                        required>
             </div>
             <div id="envoi">
-                <button type="submit" class="btn green">Envoyer</button>
+                <button type="submit" class="btn green" name="envoi">Envoyer</button>
             </div>
             <div id="reini">
                 <button type="reset" class="btn green">Réinitialiser</button>
