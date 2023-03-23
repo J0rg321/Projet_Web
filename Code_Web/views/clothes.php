b411e1ca722178195a843e9c92ffc73266bc9581
PINTO-COSTA Jorge-Miguel <Jorge-Miguel.PINTO-COSTA@cpnv.ch>
2023-03-23 16:47:04 +0100

    Update

--- Code_Web/views/clothes.php
+++ Code_Web/views/clothes.php
@@ -3,8 +3,9 @@ $title = "Vêtements";
 ?>
 
 
-<div class="col min-vh-100 p-4">
-    <button style="margin-top: 4.5%; position: fixed;" class="btn-fleche float-end" data-bs-toggle="offcanvas" data-bs-target="#offcanvas" role="button">
+<div style="filter: invert(100%)">
+    <button style="margin-top: 4.5%; positionh: fixed;" class="btn-fleche float-end" data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvas" role="button">
         <img style="width: 30px; filter: invert(100%)" src="../statics/images/fleche.png">
         <p style="color: black; ">Catégories</p>
     </button>
@@ -67,44 +68,42 @@ $title = "Vêtements";
         </form>
     </div>
 </div>
-<div class="main">
 
-    <div style="height: 5cm; width: 100%;">
 
+<?php
+// chemin d'accès à votre fichier JSON
+$file = getcwd() . '\statics\js\data.json';
+// mettre le contenu du fichier dans une variable
+$data = file_get_contents($file);
+// décoder le flux JSON
+$obj = json_decode($data, true);
+// accéder à l'élément approprié
+
+?>
+<div class="main"><?php
+  for ($i = 0;
+  $i <= count($obj) - 1;
+  $i++) {
+  $imagesVetements = $obj[$i]['articleImg'];
+  ?>
+    <div class="vetement">
+        <div id="imageVet">
+		  <?php echo "<img class='img' style='height: 500px; margin: 5px;' src='../statics/images/Vêtements/$imagesVetements/1.jpg'"; ?>
+        </div>
+        <div>
+            <div id="specsVet">
+			  <?php echo '<strong style="font-size: 30px;">' . $obj[$i]['articleName'] . '</strong>' . '<br>' . "taille : " . $obj[$i]['size'] . '<br>' . "Couleur : " . $obj[$i]['color'] . '<br>' . "Genre : " . $obj[$i]['gender'] . '<br>' . "Prix : " . $obj[$i]['price'] . " CHF"; ?>
+            </div>
+            <div id="panierVet">
+                <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor"
+                     class="bi bi-bag-heart-fill" viewBox="0 0 16 16">
+                    <path d="M11.5 4v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5ZM8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1Zm0 6.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"/>
+                </svg>
+            </div>
+        </div>
     </div>
-
-  <?php
-
-
-  // chemin d'accès à votre fichier JSON
-  $file = getcwd() . '\statics\js\data.json';
-  // mettre le contenu du fichier dans une variable
-  $data = file_get_contents($file);
-  // décoder le flux JSON
-  $obj = json_decode($data, true);
-  // accéder à l'élément approprié
-
-
-  for ($i = 0; $i <= count($obj) - 1; $i++) {
-	$imagesVetements = $obj[$i]['articleImg'];
-	?>
-      <div class="vetement">
-          <div id="imageVet">
-			<?php echo "<img class='img' style='height: 500px; margin: 5px;' src='../statics/images/Vêtements/$imagesVetements/1.jpg'"; ?>
-          </div>
-          <div>
-              <div id="specsVet">
-				<?php echo '<strong style="font-size: 30px;">' . $obj[$i]['articleName'] . '</strong>' . '<br>' . "taille : " . $obj[$i]['size'] . '<br>' . "Couleur : " . $obj[$i]['color'] . '<br>' . "Genre : " . $obj[$i]['gender'] . '<br>' . "Prix : " . $obj[$i]['price'] . " CHF"; ?>
-              </div>
-              <div id="panierVet">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-bag-heart-fill" viewBox="0 0 16 16">
-                      <path d="M11.5 4v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5ZM8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1Zm0 6.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"/>
-                  </svg>
-              </div>
-          </div>
-      </div>
 </div>
-  <?php } ?>
+<?php } ?>
 
 
 
