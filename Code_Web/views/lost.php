76864b471faf1b662fb9576b5727e62480d42a43
AlessiaMavraj <alessia.mavraj@cpnv.ch>
2023-03-28 16:53:16 +0200

    update

--- Code_Web/views/lost.php
+++ Code_Web/views/lost.php
@@ -5,7 +5,7 @@ $arrow = '';
 $title = "Lost X(";
 ?>
     <div class="mainLost" style="text-align: center">
-        <h1>Vous êtes perdu(e) 😐</h1>
+        <h1>Vous êtes perdu(e), retournez sur la page d'accueil : <a href="home.php">Ici</a></h1>
     </div>
 <?php
 $content = ob_get_clean();
