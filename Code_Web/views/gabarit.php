52fadbc240d8419230b1ac09df1f98ae3cc00239
Jorge Miguel <jorge-miguel.pinto-costa@cpnv.ch>
2023-03-28 16:43:38 +0200

    Update

--- Code_Web/views/gabarit.php
+++ Code_Web/views/gabarit.php
@@ -17,55 +17,62 @@
 <body>
 <div id="container" style="border-bottom: solid white;">
     <nav class=" navigation navbar navbar-expand-lg">
-            <div class="navigation-header">
-                <a class="logo navigation-title" href="../index.php/?action=home">
-                    <img src="../statics/images/logo.PNG" alt="WearYou" style="width:100px" ;>
+        <div class="navigation-header">
+            <a class="logo navigation-title" href="../index.php/?action=home">
+                <img src="../statics/images/logo.PNG" alt="WearYou" style="width:100px" ;>
+            </a>
+        </div>
+        <div class="navigation-btn">
+            <label for="navigation-check">
+                <span></span>
+                <span></span>
+                <span></span>
+            </label>
+        </div>
+        <div class="navbar-nav me-auto mb-1 mb-lg-0">
+            <ul class="navbar-nav">
+                <div>
+                    <a href="../index.php/?action=home">
+                        <div class="btn btn-one">
+                            <span>ACCUEIL</span>
+                        </div>
+                    </a>
+                </div>
+                <div>
+                    <a href="../index.php/?action=clothes">
+                        <div class="btn btn-one">
+                            <span>VÊTEMENTS</span>
+                        </div>
+                    </a>
+                </div>
+            </ul>
+            <form class="d-flex nav-item nav-link" role="search">
+                <div class="search-box">
+                    <button class="btn-search">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35"
+                             fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                        </svg>
+                    </button>
+                    <input type="text" class="input-search" placeholder="Rechercher">
+                </div>
+            </form>
+        </div>
+
+        <div>
+            <div style="float:left;" class="nav-item nav-link">
+                <a href="../index.php/?action=cart">
+                    <img class="nav-link icon" src="../statics/images/trolley.png" aria-current="page"
+                         style="width:100px">
                 </a>
             </div>
-            <div class="navbar-nav me-auto mb-1 mb-lg-0">
-                <ul class="navbar-nav">
-                    <div>
-                        <a href="../index.php/?action=home">
-                            <div class="btn btn-one">
-                                <span>ACCUEIL</span>
-                            </div>
-                        </a>
-                    </div>
-                    <div>
-                        <a href="../index.php/?action=clothes">
-                            <div class="btn btn-one">
-                                <span>VÊTEMENTS</span>
-                            </div>
-                        </a>
-                    </div>
-                </ul>
-                <form class="d-flex nav-item nav-link" role="search">
-                    <div class="search-box">
-                        <button class="btn-search">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35"
-                                 fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                            </svg>
-                        </button>
-                        <input type="text" class="input-search" placeholder="Rechercher">
-                    </div>
-                </form>
+            <div style="float:left;" class="nav-item nav-link">
+                <a href="../index.php/?action=login">
+                    <img class="nav-link icon" src="../statics/images/account.png" alt="WearYou"
+                         aria-current="page">
+                </a>
             </div>
-
-            <div>
-                <div style="float:left;" class="nav-item nav-link">
-                    <a href="../index.php/?action=cart">
-                        <img class="nav-link icon" src="../statics/images/trolley.png" aria-current="page"
-                             style="width:100px">
-                    </a>
-                </div>
-                <div style="float:left;" class="nav-item nav-link">
-                    <a href="../index.php/?action=login">
-                        <img class="nav-link icon" src="../statics/images/account.png" alt="WearYou"
-                             aria-current="page">
-                    </a>
-                </div>
-            </div>
+        </div>
     </nav>
 </div>
 
