e9e26ae66104f956ca386e3a666517a7f85725e6
Jorge Miguel <jorge-miguel.pinto-costa@cpnv.ch>
2023-03-30 13:40:39 +0200

    Update

--- Code_Web/views/clothes.php
+++ Code_Web/views/clothes.php
@@ -95,7 +95,7 @@ $title = "Vêtements";
 
 <?php
 // chemin d'accès à votre fichier JSON
-$file = join(DIRECTORY_SEPARATOR, array(getcwd(),'statics', 'js', 'data.json'));
+$file = join(DIRECTORY_SEPARATOR, array(getcwd(), 'statics', 'js', 'data.json'));
 
 //$file = getcwd() . '\statics\js\data.json';
 // mettre le contenu du fichier dans une variable
@@ -120,10 +120,17 @@ $obj = json_decode($data, true);
 			  <?php echo "Taille : " . $obj[$i]['size'] . '<br>' . "Couleur : " . $obj[$i]['color'] . '<br>' . "Genre : " . $obj[$i]['gender'] . '<br>' . "Prix : " . $obj[$i]['price'] . " CHF"; ?>
             </div>
             <div id="panierVet">
-                <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor"
-                     class="bi bi-bag-heart-fill" viewBox="0 0 16 16">
-                    <path d="M11.5 4v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5ZM8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1Zm0 6.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"/>
-                </svg>
+                <div class="box-1 nav-item">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor"
+                         class="bi bi-bag-heart-fill" viewBox="0 0 16 16">
+                        <path d="M11.5 4v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5ZM8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1Zm0 6.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"/>
+                    </svg>
+                </div>
+                <?php
+
+                if()
+
+                ?>
             </div>
         </div>
     </div>
--- Code_Web/views/create.php
+++ Code_Web/views/create.php
@@ -4,23 +4,39 @@ $title = "Creation de compte";
 $error = false;
 if (isset($_POST) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['username'])) {
 // chemin d'accès à votre fichier JSON
-    $file = getcwd() . '\statics\js\user.json';
+  $file = getcwd() . '\statics\js\user.json';
 // mettre le contenu du fichier dans une variable
-    $data = file_get_contents($file);
+  $data = file_get_contents($file);
 // décoder le flux JSON
-    $obj = json_decode($data, true);
+  $obj = json_decode($data, true);
 // accéder à l'élément approprié
-    print_r($obj);
-    if (!empty($obj)) {
-        for ($i = 0; $i <= count($obj) - 1; $i++) {
-            if (isset($obj[$i]['email']) && $obj[$i]['email'] == $_POST['email']) {
-                $error = true;
-            }
-        }
-        if (!$error) {
 
-<<<<<<< HEAD
-    /* AJOUTER PARTIE PANIER !!!!!!!!!!!!!*/
+  if (!empty($obj)) {
+	for ($i = 0; $i <= count($obj) - 1; $i++) {
+	  if (isset($obj[$i]['email']) && $obj[$i]['email'] == $_POST['email']) {
+		$error = true;
+	  }
+	}
+	if (!$error) {
+
+
+	// chemin d'accès à votre fichier JSON
+	  $fileCart = getcwd() . '\statics\js\cart_content.json';
+    // mettre le contenu du fichier dans une variable
+	  $dataCart = file_get_contents($fileCart);
+    // décoder le flux JSON
+	  $objCart = json_decode($dataCart, true);
+    // accéder à l'élément approprié
+
+
+	  $newCartData["email"] = $_POST['email'];
+
+	  $objCart[] = $newCartData;
+	  file_put_contents($fileCart, json_encode($objCart));
+	  //User created,
+
+
+	  /* AJOUTER PARTIE PANIER !!!!!!!!!!!!!*/
 	  $newData["username"] = $_POST['username'];
 	  $newData["email"] = $_POST['email'];
 	  $newData["password"] = password_hash($_POST['password'], PASSWORD_DEFAULT);
@@ -31,31 +47,19 @@ if (isset($_POST) && isset($_POST['email']) && isset($_POST['password']) && isse
 	  $_SESSION['username'] = $obj[$i]['username'];
 	  $_SESSION['email'] = $obj[$i]['email'];
 	  exit;
-=======
 
-            $newData["username"] = $_POST['username'];
-            $newData["email"] = $_POST['email'];
-            $newData["password"] = password_hash($_POST['password'], PASSWORD_DEFAULT);
-            $obj[] = $newData;
-            file_put_contents($file, json_encode($obj));
-            //User created,
-            header('location: /index.php/?action=home');
-            $_SESSION['username'] = $obj[$i]['username'];
-            $_SESSION['email'] = $obj[$i]['email'];
-            exit;
->>>>>>> 9848afad3e534889a8fdb1aa76810fc0d3c7c311
 
-        }
-    }
+	}
+  }
 }
 ?>
     <div class="main">
         <div class="div_centre">
             <h1>Veuillez créer votre compte : </h1>
             <form action="/index.php/?action=create" method="post">
-                <?php if ($error) {
-                    echo '<p>Ce compte est déjà existant ! Veuillez vous <a href="index.php?action=login">logger</a></p>';
-                } ?>
+			  <?php if ($error) {
+				echo '<p>Ce compte est déjà existant ! Veuillez vous <a href="index.php?action=login">logger</a></p>';
+			  } ?>
                 <div class="formLogin">
                     <div>
                         <label for="userName">Nom d'utilisateur*</label>
